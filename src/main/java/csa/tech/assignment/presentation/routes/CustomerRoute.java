b0e4230dafed7c6977b091cf6526dc95b7732e71
Stanislavs Rubens <stanislavs.rubens@leongaming.com>
2015-06-29 19:48:24 +0200

    improved Camel RESTful endpoint;

--- src/main/java/csa/tech/assignment/presentation/routes/CustomerRoute.java
+++ src/main/java/csa/tech/assignment/presentation/routes/CustomerRoute.java
@@ -3,11 +3,13 @@ package csa.tech.assignment.presentation.routes;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import csa.tech.assignment.application.customer.CustomerManager;
+import csa.tech.assignment.domain.documents.Customer;
 
 @Component
 public class CustomerRoute extends RouteBuilder {
@@ -18,15 +20,21 @@ public class CustomerRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
+        restConfiguration()
+
+        .component("restlet").port(8081).bindingMode(RestBindingMode.json);
+
         CustomerManager manager = context.getBean(CustomerManager.class);
 
-        from("restlet:http://localhost:8081/services/camel/customer?restletMethod=GET").process(new Processor() {
+        rest("/services/camel/customer").consumes("application/json").produces("application/json")
+
+        .get().outType(Customer[].class).route().process(new Processor() {
+
             @Override
             public void process(Exchange exchange) throws Exception {
-
                 exchange.getOut().setBody(manager.list());
             }
-        }).to("log:".concat(CustomerRoute.class.getName()));
 
+        }).to("log:".concat(CustomerRoute.class.getName()));
     }
 }
